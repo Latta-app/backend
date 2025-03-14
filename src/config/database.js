18615f97530a0c7e336a60b37af347d629be2348
Rafael Souza <souza_rafael.97@outlook.com>
2025-03-13 22:04:56 -0300

    refactor: connection database

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,19 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import pg from 'pg';
 
 dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
-  }
+      rejectUnauthorized: false,
+    },
+    family: 4,
+  },
 });
 
 export const checkDatabaseConnection = async () => {
